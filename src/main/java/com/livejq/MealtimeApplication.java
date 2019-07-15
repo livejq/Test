90c61a5f8d8593c14cdcf6730ad49317e511c8fe
livejq <1325286933@qq.com>
2019-07-15 18:05:18 +0800

    老王的第一天

---
+++ src/main/java/com/livejq/MealtimeApplication.java
@@ -0,0 +1,13 @@
+package com.livejq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealtimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealtimeApplication.class, args);
+    }
+
+}
