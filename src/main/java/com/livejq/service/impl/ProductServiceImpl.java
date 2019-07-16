82048acda0900e0f59c8de3650e13344cfbc3f8b
livejq <1325286933@qq.com>
2019-07-16 22:25:33 +0800

    老王的第二天

--- src/main/java/com/livejq/service/impl/ProductServiceImpl.java
+++ src/main/java/com/livejq/service/impl/ProductServiceImpl.java
@@ -43,69 +43,69 @@ public class ProductServiceImpl implements ProductService {
         return repository.save(productInfo);
     }
 
-//    @Override
-//    @Transactional
-//    public void increaseStock(List<CartDTO> cartDTOList) {
-//        for (CartDTO cartDTO: cartDTOList) {
-//            ProductInfo productInfo = repository.findOne(cartDTO.getProductId());
-//            if (productInfo == null) {
-//                throw new SellException(ResultEnum.PRODUCT_NOT_EXIST);
-//            }
-//            Integer result = productInfo.getProductStock() + cartDTO.getProductQuantity();
-//            productInfo.setProductStock(result);
-//
-//            repository.save(productInfo);
-//        }
-//
-//    }
+   @Override
+   @Transactional
+   public void increaseStock(List<CartDTO> cartDTOList) {
+       for (CartDTO cartDTO: cartDTOList) {
+           ProductInfo productInfo = repository.findOne(cartDTO.getProductId());
+           if (productInfo == null) {
+               throw new SellException(ResultEnum.PRODUCT_NOT_EXIST);
+           }
+           Integer result = productInfo.getProductStock() + cartDTO.getProductQuantity();
+           productInfo.setProductStock(result);
 
-//    @Override
-//    @Transactional
-//    public void decreaseStock(List<CartDTO> cartDTOList) {
-//        for (CartDTO cartDTO: cartDTOList) {
-//            ProductInfo productInfo = repository.findOne(cartDTO.getProductId());
-//            if (productInfo == null) {
-//                throw new SellException(ResultEnum.PRODUCT_NOT_EXIST);
-//            }
-//
-//            Integer result = productInfo.getProductStock() - cartDTO.getProductQuantity();
-//            if (result < 0) {
-//                throw new SellException(ResultEnum.PRODUCT_STOCK_ERROR);
-//            }
-//
-//            productInfo.setProductStock(result);
-//
-//            repository.save(productInfo);
-//        }
-//    }
+           repository.save(productInfo);
+       }
 
-//    @Override
-//    public ProductInfo onSale(String productId) {
-//        ProductInfo productInfo = repository.findOne(productId);
-//        if (productInfo == null) {
-//            throw new SellException(ResultEnum.PRODUCT_NOT_EXIST);
-//        }
-//        if (productInfo.getProductStatusEnum() == ProductStatusEnum.UP) {
-//            throw new SellException(ResultEnum.PRODUCT_STATUS_ERROR);
-//        }
-//
-//        //更新
-//        productInfo.setProductStatus(ProductStatusEnum.UP.getCode());
-//        return repository.save(productInfo);
-//    }
-//
-//    @Override
-//    public ProductInfo offSale(String productId) {
-//        ProductInfo productInfo = repository.findOne(productId);
-//        if (productInfo == null) {
-//            throw new SellException(ResultEnum.PRODUCT_NOT_EXIST);
-//        }
-//        if (productInfo.getProductStatusEnum() == ProductStatusEnum.DOWN) {
-//            throw new SellException(ResultEnum.PRODUCT_STATUS_ERROR);
-//        }
-//
-//        //更新
-//        productInfo.setProductStatus(ProductStatusEnum.DOWN.getCode());
-//        return repository.save(productInfo);
-//    }
+   }
+
+   @Override
+   @Transactional
+   public void decreaseStock(List<CartDTO> cartDTOList) {
+       for (CartDTO cartDTO: cartDTOList) {
+           ProductInfo productInfo = repository.findOne(cartDTO.getProductId());
+           if (productInfo == null) {
+               throw new SellException(ResultEnum.PRODUCT_NOT_EXIST);
+           }
+
+           Integer result = productInfo.getProductStock() - cartDTO.getProductQuantity();
+           if (result < 0) {
+               throw new SellException(ResultEnum.PRODUCT_STOCK_ERROR);
+           }
+
+           productInfo.setProductStock(result);
+
+           repository.save(productInfo);
+       }
+   }
+
+   @Override
+   public ProductInfo onSale(String productId) {
+       ProductInfo productInfo = repository.findOne(productId);
+       if (productInfo == null) {
+           throw new SellException(ResultEnum.PRODUCT_NOT_EXIST);
+       }
+       if (productInfo.getProductStatusEnum() == ProductStatusEnum.UP) {
+           throw new SellException(ResultEnum.PRODUCT_STATUS_ERROR);
+       }
+
+       //更新
+       productInfo.setProductStatus(ProductStatusEnum.UP.getCode());
+       return repository.save(productInfo);
+   }
+
+   @Override
+   public ProductInfo offSale(String productId) {
+       ProductInfo productInfo = repository.findOne(productId);
+       if (productInfo == null) {
+           throw new SellException(ResultEnum.PRODUCT_NOT_EXIST);
+       }
+       if (productInfo.getProductStatusEnum() == ProductStatusEnum.DOWN) {
+           throw new SellException(ResultEnum.PRODUCT_STATUS_ERROR);
+       }
+
+       //更新
+       productInfo.setProductStatus(ProductStatusEnum.DOWN.getCode());
+       return repository.save(productInfo);
+   }
 }
